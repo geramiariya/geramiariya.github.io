99960cc36bf77c10eebf2d267da837bf4efec8ef
geramiariya <166827276+geramiariya@users.noreply.github.com>
2024-04-12 09:58:15 -0400

    Create form-handler.php

---
+++ form-handler.php
@@ -0,0 +1,15 @@
+<form action="form-handler.php" method="POST">
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" required><br><br>
+
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email" required><br><br>
+
+  <label for="phone">Phone:</label>
+  <input type="tel" id="phone" name="phone"><br><br>
+
+  <label for="message">Message:</label><br>
+  <textarea id="message" name="message" rows="4" required></textarea><br><br>
+
+  <input type="submit" value="Submit">
+</form>
